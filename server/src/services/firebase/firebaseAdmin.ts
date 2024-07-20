212d69fca52142d5ccadb14efe01ed46502cc283
Filipe Maciel <filipemleicht@gmail.com>
2024-07-20 14:15:42 -0300

    updated firebase credentials

--- server/src/services/firebase/firebaseAdmin.ts
+++ server/src/services/firebase/firebaseAdmin.ts
@@ -3,7 +3,7 @@ import { getFirestore } from 'firebase-admin/firestore';
 import { initializeApp, getApps } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
-const serviceAccount = require('./credentials/nia-pixel-show-firebase-adminsdk-d7h8l-9cb06f05c1.json');
+const serviceAccount = require('./credentials/nia-pixel-show---testes-firebase-adminsdk-8m0ty-ee554cb33c.json');
 
 if (!getApps().length) {
   initializeApp({
